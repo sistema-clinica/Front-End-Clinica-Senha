392da27dbb8f6a0a7e0978a9ca45d8f0d355ebbb
Allan Cauê R. Moura <allancaue@gmail.com>
2024-03-15 08:17:59 -0300

    fix: error correction
    When I created the branch I forgot to fix a test change I had made

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Login from './page/login';
-import Cadastro from './page/cadastro';
-import MenuPrincipal from './page/menu-principal';
-import PainelSenhas from './page/painel-senhas';
+import RoutesApp from "./routes/routesApp";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/'>
-          <Login/>
-        </Route>
-        <Route path='/Cadastro'>
-          <Cadastro/>
-        </Route>
-        <Route path='/Menu'>
-          <MenuPrincipal/>
-        </Route>
-        <Route path='/PainelSenhas'>
-          <PainelSenhas/>
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <RoutesApp/>
   );
 }
 
