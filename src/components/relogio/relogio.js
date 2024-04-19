60fac9ab2d62d538b76b45c5e9708d3df04e8f18
joaoarthur <joaoabritto.dev@gmail.com>
2024-04-19 07:01:21 -0300

    refactor: Changed the structure of the folders

--- src/components/relogio/relogio.js
+++ src/components/relogio/relogio.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './relogio.module.css';
 import moment from "moment";
 
--- src/services/fumcoes.js
+++
@@ -1,3 +0,0 @@
-const API_URL = "http://ec2-54-174-184-107.compute-1.amazonaws.com:8080";
-
-export const url = API_URL;
