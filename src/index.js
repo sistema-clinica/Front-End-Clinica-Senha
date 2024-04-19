3a02f52ca0f002008eb5c5b59bc70ba61bcbd119
joaoarthur <joaoabritto.dev@gmail.com>
2024-04-19 06:58:41 -0300

    refactor: Refactored the react router implementation

--- src/index.js
+++ src/index.js
@@ -2,10 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import PainelSenhas from "./page/painel-senha/painel-senhas";
+import Login from "./page/login/login";
+import Cadastro from "./page/cadastro/cadastro";
+import MenuPrincipal from "./page/menu-principal/menu-principal";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        children: [
+            {
+                path: "painel/senhas",
+                element: <PainelSenhas/>,
+            },
+            {
+                path: "login",
+                element: <Login/>,
+            },
+            {
+                path: "cadastro",
+                element: <Cadastro/>
+            },
+            {
+                path: "",
+                element: <MenuPrincipal/>,
+            }
+        ]
+    }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
